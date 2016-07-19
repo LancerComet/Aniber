23d69f51508cd314e2de38c7eea87b37b828635c
LancerComet <chw644@hotmail.com>
2016-07-19 20:55:51 +0800

    + Add IE web driver.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,18 +2,22 @@
  *  Nightwatch Configuration By LancerComet at 0:51, 2016/7/17.
  *  # Carry Your World #
  *  ---
- *  Nightwatch 配置文件.
+ *  Nightwatch Configuration file.
+ *  Loading settings in "./build" and attach them to Nightwatch. 
  */
 
 const seleniumConfig = require('./build/selenium-conf')
 const path = require('path')
 
 module.exports = (function (settings) {
-    // 设置 Selenium 所在位置。
+    // Tell Nightwatch where is the Selenium. | 设置 Selenium 所在位置。
     settings.selenium.server_path = `${path.resolve()}/node_modules/selenium-standalone/.selenium/selenium-server/${seleniumConfig.selenium.version}-server.jar`
 
-    // 加载 Chrome Driver.
+    // Attach Chrome web driver. | 加载 Chrome Driver.
     settings.selenium.cli_args['webdriver.chrome.driver'] = `${path.resolve()}/node_modules/selenium-standalone/.selenium/chromedriver/${seleniumConfig.driver.chrome.version}-${seleniumConfig.driver.chrome.arch}-chromedriver`
+
+    // Attach IE web driver. | 加载 IE Driver.
+    settings.selenium.cli_args['webdriver.ie.driver'] = `${path.resolve()}/node_modules/selenium-standalone/.selenium/iedriver/${seleniumConfig.driver.ie.version}-${seleniumConfig.driver.ie.arch}-IEDriverServer.exe`
     
   return settings
 })(require('./nightwatch.json'))
